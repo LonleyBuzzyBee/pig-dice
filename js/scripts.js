05144acd95b2d2aa8b94982f3ca1ec5937c387f2
Student <example@example.com>
2020-01-30 10:43:38 -0800

    add debugging in scripts.js form button added index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
-function p1() {
+function Pc() {
+  this.playerName,
   this.roll1 = 0
 }
 
-function initToss() {
-  var rando =  Math.floor(Math.random() * 6) + 1;
-        console.log(rando);
-        return this.roll1 + rando
+Pc.prototype.initToss = function() {
+  Math.floor(Math.random() * 6) + 1;
+    return this.roll1
     
       }
   
 
 
-
-// function finalScore() {
-
-
-
-
+// // toss button
+//   tossInto() {
+//     var diceRoll = $(".btn").val();
+//     initToss(diceRoll);
 
 
 
 
 
-$(document).ready(function() {
-  $(".btn").click(function(event) {
-    event.preventDefault();
-    var diceRoll = $(".btn").val();
-    initToss(diceRoll);
+
+//start page
+$(document).ready(function() {  
+  event.preventDefault();
+  $("form").submit(function(event){
+  var name = $("form").val();
+  pTurn = New Pc($("input.name1").val());
+  $(".namepc").text(pTurn.name);
+  $(".start").show();
+  $(".play").hide();
   });
+  // toss button
+$(".btn").click(function() {
+  var playerRoll = initToss();
+
+} 
 });
